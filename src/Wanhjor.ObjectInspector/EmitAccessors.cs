d3bf888180cdde08e703d61fd05d2a2a4bba7d83
Daniel Redondo <tonyredondo@gmail.com>
2020-05-21 23:06:17 +0200

    fixes some il

--- src/Wanhjor.ObjectInspector/EmitAccessors.cs
+++ src/Wanhjor.ObjectInspector/EmitAccessors.cs
@@ -12,18 +12,30 @@ namespace Wanhjor.ObjectInspector
         public static Func<object, object> BuildGetAccessor(PropertyInfo property)
         {
             var getPropMethod = new DynamicMethod($"GetProp+{property.DeclaringType.Name}.{property.Name}", typeof(object), new[] {typeof(object)}, typeof(EmitAccessors).Module);
-
             var il = getPropMethod.GetILGenerator();
             if (property.GetMethod.IsStatic)
             {
                 il.EmitCall(OpCodes.Call, property.GetMethod, null);
+                if (property.DeclaringType.IsValueType)
+                    il.Emit(OpCodes.Box, property.PropertyType);
                 il.Emit(OpCodes.Ret);
             }
             else
             {
                 il.Emit(OpCodes.Ldarg_0);
-                il.Emit(property.DeclaringType.IsValueType ? OpCodes.Unbox_Any : OpCodes.Castclass, property.DeclaringType);
-                il.EmitCall(OpCodes.Call, property.GetMethod, null);
+                if (property.DeclaringType.IsValueType)
+                {
+                    il.Emit( OpCodes.Unbox_Any, property.DeclaringType);
+                    il.Emit( OpCodes.Stloc_0);
+                    il.Emit( OpCodes.Ldloca_S, 0);
+                    il.EmitCall(OpCodes.Callvirt, property.GetMethod, null);
+                    il.Emit(OpCodes.Box, property.PropertyType);
+                }
+                else
+                {
+                    il.Emit(OpCodes.Castclass, property.DeclaringType);
+                    il.EmitCall(OpCodes.Callvirt, property.GetMethod, null);
+                }
                 il.Emit(OpCodes.Ret);
             }
 
