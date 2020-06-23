b19b3890958f5b7305bd29aa03d045904ff07118
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-06-23 18:29:02 +0200

    DuckLeasing performance improvements and bump to 0.4.0 version

--- src/Wanhjor.ObjectInspector/DuckTypeLeasing.cs
+++ src/Wanhjor.ObjectInspector/DuckTypeLeasing.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Threading;
 
 namespace Wanhjor.ObjectInspector
 {
@@ -9,6 +10,7 @@ namespace Wanhjor.ObjectInspector
     public ref struct DuckTypeLeasing<TInterface> where TInterface:class
     {
         private static readonly ConcurrentStack<TInterface> Proxies = new ConcurrentStack<TInterface>();
+        private static TInterface? _firstItem;
         
         /// <summary>
         /// Current duck type instance
@@ -24,12 +26,20 @@ namespace Wanhjor.ObjectInspector
             var inst = Instance;
             Instance = default!;
             ((ISettableDuckType) inst).SetInstance(null!);
+            if (_firstItem == null)
+            {
+                _firstItem = inst;
+                return;
+            }
             Proxies.Push(inst);
         }
 
         internal static DuckTypeLeasing<TInterface> Rent(IDuckTypeFactory<TInterface> factory, object instance)
         {
-            if (!Proxies.TryPop(out var proxy))
+            var proxy = _firstItem;
+            if (proxy != null && proxy == Interlocked.CompareExchange(ref _firstItem, null, proxy))
+                ((ISettableDuckType)proxy).SetInstance(instance);
+            else if (!Proxies.TryPop(out proxy))
                 proxy = factory.Create(instance);
             else
                 ((ISettableDuckType)proxy).SetInstance(instance);
@@ -40,7 +50,17 @@ namespace Wanhjor.ObjectInspector
         }
         internal static DuckTypeLeasing<IDuckType> RentDuckType(IDuckTypeFactory factory, object instance)
         {
-            if (!(Proxies.TryPop(out var proxy) && proxy is ISettableDuckType dtProxy))
+            var proxy = _firstItem;
+            if (proxy != null && proxy == Interlocked.CompareExchange(ref _firstItem, null, proxy) && proxy is ISettableDuckType sProxy)
+            {
+                sProxy.SetInstance(instance);
+                return new DuckTypeLeasing<IDuckType>
+                {
+                    Instance = sProxy
+                };
+            }
+
+            if (!(Proxies.TryPop(out proxy) && proxy is ISettableDuckType dtProxy))
                 dtProxy = (ISettableDuckType) factory.Create(instance);
             else
                 dtProxy.SetInstance(instance);
